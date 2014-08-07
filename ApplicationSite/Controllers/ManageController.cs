e83d99d0b5450e19e5466509351932de5094fed7
Tim Miller <innerlogic4321@gmail.com>
2014-08-06 22:58:48 -0400

    Replace Manage Pages

--- ApplicationSite/Controllers/ManageController.cs
+++ ApplicationSite/Controllers/ManageController.cs
@@ -16,7 +16,24 @@ namespace ApplicationSite.Controllers
         /// Gets the index page.
         /// </summary>
         /// <returns>An action result.</returns>
+        [Authorize(Roles = "Admin,Employee,Candidate")]
         public ActionResult Index()
+        {
+            // If the user is an employee or the admin, launch the employee admin page
+            // or else it's a normal user, so launch the candidate management page.
+            if (User.IsInRole("Employee") || User.IsInRole("Admin"))
+            {
+               return RedirectToAction("ManageEmployee");
+            }
+           return RedirectToAction("ManageCandidate");
+        }
+
+        public ActionResult ManageEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult ManageCandidate()
         {
             return View();
         }
