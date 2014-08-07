6db59449b357174d9ac50f482e3c8cc6b4e5b316
Tim Miller <innerlogic4321@gmail.com>
2014-08-07 19:09:54 -0400

    Add Position List Views
    * Users can now view positions in a list group, view the details of a
    position, and apply for one.
    * Basic service side validation is in place for the position detail
    page. If the position is closed, a user should not be able to view it.

---
+++ ApplicationSite/Controllers/PositionsListController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationSite.Models;
+using ApplicationSite.ViewModels;
+
+namespace ApplicationSite.Controllers
+{
+    public class PositionsListController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: PositionsList
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var positions = _db.Positions.Where(node => node.PositionStatus == PositionStatus.Open).ToList();
+            var positionListVm = new PositionsListViewModel();
+            positionListVm.MapTo(positions);
+            return View(positionListVm);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Details(int id)
+        {
+            var position = _db.Positions.Find(id);
+            if (position.PositionStatus != PositionStatus.Open)
+            {
+                // If the user saved a link to the position detail page, and it has since been closed
+                // We need to kick them out.
+                // TODO: Create new error page telling the user that the position is closed.
+                return RedirectToAction("Index");
+            }
+            return View(position);
+        }
+    }
+}
---
+++ ApplicationSite/ViewModels/PositionsListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ApplicationSite.Models;
+
+namespace ApplicationSite.ViewModels
+{
+    public class PositionsListViewModel
+    {
+        public List<Positions> PositionsList { get; set; }
+
+        public void MapTo(List<Positions> positions)
+        {
+            this.PositionsList = positions;
+        }
+    }
+}
