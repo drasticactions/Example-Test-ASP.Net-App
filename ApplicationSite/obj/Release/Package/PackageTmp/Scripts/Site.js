21d6304c163607ef3d6377a16c6b10bf49fff612
Tim Miller <innerlogic4321@gmail.com>
2014-08-08 21:37:51 -0400

    Add Edit Resume to ManageEmployee View

---
+++ ApplicationSite/obj/Release/Package/PackageTmp/Scripts/Site.js
@@ -0,0 +1,10 @@
+﻿// TODO: Testing as a global function.
+function setupSiteModal(containerName, modalName) {
+    var url = $(modalName).data('url');
+
+    $.get(url, function(data) {
+        $(containerName).html(data);
+
+        $(modalName).modal('show');
+    });
+};
