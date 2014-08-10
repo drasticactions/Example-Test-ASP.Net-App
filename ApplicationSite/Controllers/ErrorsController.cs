ee26b738577e10fd7c539c9e2f54af46c3ccd095
Tim Miller <innerlogic4321@gmail.com>
2014-08-10 10:13:35 -0400

    And basic error views.

---
+++ ApplicationSite/Controllers/ErrorsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApplicationSite.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        /// <summary>
+        /// Shows the internal error page.
+        /// </summary>
+        /// <returns>An action result.</returns>
+        public ActionResult InternalError()
+        {
+            // NOTE: This is a basic view, with no error information. If an error occurrs, we should capture it.
+            // so we can figure out what went wrong.
+            return View();
+        }
+
+        /// <summary>
+        /// Shows the 404 page not found.
+        /// </summary>
+        /// <returns>An action result.</returns>
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
